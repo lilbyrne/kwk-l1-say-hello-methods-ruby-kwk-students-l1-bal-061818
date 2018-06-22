ebc008584e254c506596e12d6caf830e0c325bc3
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-22 01:35:16 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -5,3 +5,9 @@ def say_hello (name = "Ruby Programer")
 end
 say_hello
 
+#if you just run what is above, it will say "Hell oRuby Programmer!" because that is the default argument"
+
+def say_hello (name = "Ruby Programer")
+  puts "Hello #{name}!"
+end
+say_hello(Lily)
