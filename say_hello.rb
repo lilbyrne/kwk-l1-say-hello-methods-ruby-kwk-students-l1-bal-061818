a79ceb2cd5582f3372829a5bd0f8a75a860c9e85
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-22 01:36:46 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,6 +3,13 @@
 def say_hello (name = "Ruby Programer")
   puts "Hello #{name}!"
 end
+say_hello
+
+#if you just run what is above, it will say "Hell oRuby Programmer!" because that is the default argument"
+
+def say_hello (name = "Ruby Programer")
+  puts "Hello #{name}!"
+end
 say_hello("Lily")
 
-#if you run what is above
+#if you run what is above it will say "Hello Lily because it will override the default argument because you added a new argument"
