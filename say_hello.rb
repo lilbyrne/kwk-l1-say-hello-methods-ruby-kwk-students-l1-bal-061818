309a9bfa8adc421d0249848c877da5e3469ddce4
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-22 01:35:48 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,8 @@
-# Build your say_hello method here
 
-def say_hello (name = "Ruby Programer")
-  puts "Hello #{name}!"
-end
-say_hello
-
-#if you just run what is above, it will say "Hell oRuby Programmer!" because that is the default argument"
 
 def say_hello (name = "Ruby Programer")
   puts "Hello #{name}!"
 end
 say_hello(Lily)
+
+#if you run what is above
